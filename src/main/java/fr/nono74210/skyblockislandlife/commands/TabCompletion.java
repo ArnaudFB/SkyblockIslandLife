12021cbd3459f0352228d05672002e15918c76aa
ArnaudFB <favrebonvinarnaud@gmail.com>
2024-01-11 18:04:10 +0100

    TabCompleter completed

--- src/main/java/fr/nono74210/skyblockislandlife/commands/TabCompletion.java
+++ src/main/java/fr/nono74210/skyblockislandlife/commands/TabCompletion.java
@@ -1,8 +1,10 @@
 package fr.nono74210.skyblockislandlife.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
@@ -11,17 +13,29 @@ import java.util.List;
 
 public class TabCompletion implements TabCompleter {
 
-    private static final List<String> COMMANDS = new ArrayList<String>(Arrays.asList("add", "reload"));
-
+    List<String> arguments = new ArrayList<>(Arrays.asList("add", "reload"));
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
 
-        final List<String> completions = new ArrayList<>();
+        List<String> result = new ArrayList<>();
 
-        StringUtil.copyPartialMatches(args[0], COMMANDS, completions);
+        if (args.length == 1) {
+            for (String string : arguments) {
+                if (string.toLowerCase().startsWith(args[0].toLowerCase())) {
+                    result.add(string);
+                }
+                return result;
+            }
+        }
 
-        return completions;
-
+        if (args.length == 2) {
+            for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+                if (player.getName().toLowerCase().startsWith(args[1].toLowerCase())) {
+                    result.add(player.getName());
+                }
+                return result;
+            }
+        }
+        return null;
     }
-
 }
